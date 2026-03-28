05e7b7e58383b323a24f5e72a03d4f5f2b4486b2
Stefani Ritter <ritterstefani666@gmail.com>
2026-03-27 22:19:21 -0300

    Aula 4 - Adicionando catálogo de filmes

---
+++ catalogo/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Divônicos",
+        items: [
+            {
+                img: "https://th.bing.com/th/id/OIF.gEn0shAZVoYsIWkyKUcf2w?w=317&h=180&c=7&r=0&o=7&pid=1.7&rm=3", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=6ZOZwUQKu3E"
+            },
+            {
+                img: "https://akamai.sscdn.co/uploadfile/letras/playlists/e/8/c/1/e8c13d0c5fc247c89dc9667c7a19febf.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=Mgjwq1ZzdPQ"
+            },
+            {
+                img: "https://biznewsasia.com/wp-content/uploads/2020/01/Musical-Movies-Pics-22-page-004-678x381.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=s_cJ7WmDzIQ"
+            },
+            {
+                img: "https://www.em.com.br/emfoco/wp-content/uploads/2025/03/Legalmente-Loira.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=vWOHwI_FgAo"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    }
+];
